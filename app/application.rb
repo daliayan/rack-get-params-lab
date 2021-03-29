93cf56213fca77a4c8a5991f3226b9762cce50f5
daliayan <daliayan5@gmail.com>
2021-03-29 16:23:35 -0400

    creating parameters / rack routes

--- app/application.rb
+++ app/application.rb
@@ -1,30 +1,46 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      @@items.each do |item|
-        resp.write "#{item}\n"
+        @@items.each do |item|
+          resp.write "#{item}\n"
       end
     elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
+        search_term = req.params["q"]
+        resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+       if @@cart.empty?
+          resp.write "Your cart is empty."
+        else
+          @@cart.each do |items|
+            resp.write "#{items}\n"
+        end
+      end
+    elsif req.path.match(/add/)
+      item_added = req.params["item"]
+        if @@items.include?(item_added)
+          @@cart << item_added
+          resp.write "added #{item_added}"
+        else
+          resp.write "We don't have that item"
+        end
     else
-      resp.write "Path Not Found"
+      resp.write "Path not found"
     end
-
-    resp.finish
+      resp.finish
   end
 
   def handle_search(search_term)
     if @@items.include?(search_term)
-      return "#{search_term} is one of our items"
+       return "#{search_term} is one of our items"
     else
-      return "Couldn't find #{search_term}"
+     return "Couldn't find #{search_term}"
     end
   end
 end
